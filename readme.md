a57eaa340b88a93fe8e9be50ed048d88299dacfc
tamami <tamami.oka@gmail.com>
2016-12-17 15:10:52 +0700

    init commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# belajar integrasi mockito with junit
+
+sumber : https://www.tutorialspoint.com/mockito/mockito_junit_integration.htm
--- src/main/java/lab/aikibo/services/CalculatorService.java
+++ src/main/java/lab/aikibo/services/CalculatorService.java
@@ -4,4 +4,10 @@ package lab.aikibo.services;
  * Created by tamami on 17/12/16.
  */
 public interface CalculatorService {
+
+    public double add(double input1, double input2);
+    public double subtract(double input1, double input2);
+    public double multiply(double input1, double input2);
+    public double divide(double input1, double input2);
+
 }
